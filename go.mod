de1bb9f5be06a1f66388e6ac0c08d3152a851091
rekby <timofey@koolin.ru>
2019-04-12 21:15:29 +0300

    Update zapcontext

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/pelletier/go-toml v1.3.0
-	github.com/rekby/zapcontext v0.0.2
+	github.com/rekby/zapcontext v0.0.3
+	go.uber.org/zap v1.9.1
 )
--- vendor/github.com/rekby/zapcontext/context.go
+++ vendor/github.com/rekby/zapcontext/context.go
@@ -7,17 +7,40 @@ import (
 
 type loggerContextMark struct{}
 
-// Return copy of ctx with assiciated logger
+var (
+	defaultLogger        *zap.Logger
+	defaultSugaredLogger *zap.SugaredLogger
+	nopLogger            = zap.NewNop()
+	nopSugar             = zap.NewNop().Sugar()
+)
+
+// SetDefaultLogger set logger, which will return with L and S function if no logger in context.
+// It is NOT thread safe and no syncronization with L and S
+func SetDefaultLogger(logger *zap.Logger) {
+	defaultLogger = logger
+	if logger == nil {
+		defaultSugaredLogger = nil
+	} else {
+		defaultSugaredLogger = logger.Sugar()
+	}
+}
+
+// Return copy of ctx with associated logger
 func WithLogger(ctx context.Context, l *zap.Logger) context.Context {
 	return context.WithValue(ctx, loggerContextMark{}, l)
 }
 
+// Return copy of ctx with associated logger
 func WithSugarLogger(ctx context.Context, l *zap.SugaredLogger) context.Context {
 	return context.WithValue(ctx, loggerContextMark{}, l)
 }
 
-// Return associated logger. Nop if none.
+// Return associated logger. Default logger if no ctx logger yet. (default of default is nil)
 func L(ctx context.Context) *zap.Logger {
+	if ctx == nil {
+		return defaultLogger
+	}
+
 	val := ctx.Value(loggerContextMark{})
 	if logger, ok := val.(*zap.Logger); ok {
 		return logger
@@ -25,18 +48,31 @@ func L(ctx context.Context) *zap.Logger {
 	if logger, ok := val.(*zap.SugaredLogger); ok {
 		return logger.Desugar()
 	}
-	return nil
+	return defaultLogger
 }
 
+// Return associated logger. Nop if no ctx logger yet.
 func LNop(ctx context.Context) (res *zap.Logger) {
-	res = L(ctx)
-	if res == nil {
-		res = zap.NewNop()
+	if ctx == nil {
+		return nopLogger
 	}
-	return res
+
+	val := ctx.Value(loggerContextMark{})
+	if logger, ok := val.(*zap.Logger); ok {
+		return logger
+	}
+	if logger, ok := val.(*zap.SugaredLogger); ok {
+		return logger.Desugar()
+	}
+	return nopLogger
 }
 
+// Return associated logger. Default logger if no ctx logger yet. (default of default is nil)
 func S(ctx context.Context) *zap.SugaredLogger {
+	if ctx == nil {
+		return defaultSugaredLogger
+	}
+
 	res := ctx.Value(loggerContextMark{})
 	if logger, ok := res.(*zap.SugaredLogger); ok {
 		return logger
@@ -44,13 +80,21 @@ func S(ctx context.Context) *zap.SugaredLogger {
 	if logger, ok := res.(*zap.Logger); ok {
 		return logger.Sugar()
 	}
-	return nil
+	return defaultSugaredLogger
 }
 
-func SNop(ctx context.Context) (res *zap.SugaredLogger) {
-	res = S(ctx)
-	if res == nil {
-		res = zap.NewNop().Sugar()
+// Return associated logger. Nop if no ctx logger yet.
+func SNop(ctx context.Context) *zap.SugaredLogger {
+	if ctx == nil {
+		return nopSugar
 	}
-	return res
+
+	res := ctx.Value(loggerContextMark{})
+	if logger, ok := res.(*zap.SugaredLogger); ok {
+		return logger
+	}
+	if logger, ok := res.(*zap.Logger); ok {
+		return logger.Sugar()
+	}
+	return nopSugar
 }
