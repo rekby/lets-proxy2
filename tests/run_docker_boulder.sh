4378d0e2f9882f42053ea793b6aa4e98bdbb679a
rekby <timofey@koolin.ru>
2019-04-13 19:49:11 +0300

    fake dns

--- tests/run_docker_boulder.sh
+++ tests/run_docker_boulder.sh
@@ -2,7 +2,10 @@
 GOPATH=$(go env GOPATH)
 git clone https://github.com/letsencrypt/boulder/ $GOPATH/src/github.com/letsencrypt/boulder
 cd $GOPATH/src/github.com/letsencrypt/boulder
-docker-compose up -d
+
+docker-compose build
+
+docker-compose run -d --use-aliases -e FAKE_DNS=172.17.0.1 --service-ports boulder ./start.py
 
 while ! curl -q http://localhost:4000 >/dev/null 2>&1; do
     echo -n "Wait for bounder start listen "
