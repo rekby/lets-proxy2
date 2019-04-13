f317d98474e270afc045718a3a9c8156648dd79f
rekby <timofey@koolin.ru>
2019-04-13 20:18:44 +0300

    always default go version for boulder try build with tip go

--- tests/run_docker_boulder.sh
+++ tests/run_docker_boulder.sh
@@ -5,7 +5,9 @@ GOPATH=$(go env GOPATH)
 git clone https://github.com/letsencrypt/boulder/ $GOPATH/src/github.com/letsencrypt/boulder
 cd $GOPATH/src/github.com/letsencrypt/boulder
 
-sed -i -e 's/FAKE_DNS.*/FAKE_DNS: 172.17.0.1/' docker-compose.yml
+sed -i -e 's/FAKE_DNS.*/FAKE_DNS: 172.17.0.1/' docker-compose.yml # Fake dns to docker host
+
+sed -i -e 's/TRAVIS_GO_VERSION/TRAVIS_GO_VERSION_OFF/' docker-compose.yml # always build boulder with default go version
 
 docker-compose up -d
 
