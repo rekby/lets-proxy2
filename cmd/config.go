62c2da24e85f6538aacd14a2902344e1c1464a5a
rekby <timofey@koolin.ru>
2019-05-25 22:39:10 +0300

    Allow include config files Strict toml read config Close #70

--- cmd/config.go
+++ cmd/config.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	"github.com/gobuffalo/packr"
 
@@ -20,10 +25,12 @@ import (
 )
 
 type ConfigGeneral struct {
-	IssueTimeout      int
-	StorageDir        string
-	AcmeServer        string
-	StoreJSONMetadata bool
+	IssueTimeout       int
+	StorageDir         string
+	AcmeServer         string
+	StoreJSONMetadata  bool
+	IncludeConfigs     []string
+	MaxConfigFilesRead int
 }
 
 //go:generate packr
@@ -50,15 +57,20 @@ type logConfig struct {
 }
 
 var (
-	_config *configType
+	_config           *configType
+	parsedConfigFiles = 0
 )
 
 func getConfig(ctx context.Context) *configType {
 	if _config == nil {
 		logger := zc.LNop(ctx).With(zap.String("config_file", *configFileP))
 		logger.Info("Read config")
-		_config = readConfig(ctx, *configFileP)
+		_config = &configType{}
+		mergeConfigBytes(ctx, _config, defaultConfig(ctx), "default")
+		mergeConfigByTemplate(ctx, _config, *configFileP)
 		applyFlags(ctx, _config)
+		logger.Info("Parse configs finished", zap.Int("readed_files", parsedConfigFiles),
+			zap.Int("max_read_files", _config.General.MaxConfigFilesRead))
 	}
 	return _config
 }
@@ -94,4 +106,81 @@ func readConfig(ctx context.Context, file string) *configType {
 		log.InfoFatal(logger, err, "Parse config file", zap.String("filepath", file))
 	}
 	return &res
+}
+
+func mergeConfigByTemplate(ctx context.Context, c *configType, filepathTemplate string) {
+	logger := zc.LNop(ctx).With(zap.String("config_file", filepathTemplate))
+	if !hasMeta(filepathTemplate) {
+		mergeConfigByFilepath(ctx, c, filepathTemplate)
+		return
+	}
+
+	filenames, err := filepath.Glob(filepathTemplate)
+	log.DebugFatal(logger, err, "Expand config file template",
+		zap.String("filepathTemplate", filepathTemplate), zap.Strings("files", filenames))
+	for _, filename := range filenames {
+		mergeConfigByFilepath(ctx, c, filename)
+	}
+}
+
+func mergeConfigByFilepath(ctx context.Context, c *configType, filename string) {
+	logger := zc.LNop(ctx).With(zap.String("config_file", filename))
+	if parsedConfigFiles > c.General.MaxConfigFilesRead {
+		logger.Fatal("Exceed max config files read count", zap.Int("MaxConfigFilesRead", c.General.MaxConfigFilesRead))
+	}
+	parsedConfigFiles++
+
+	var err error
+	if !filepath.IsAbs(filename) {
+		var filepathNew string
+		filepathNew, err = filepath.Abs(filename)
+		log.DebugFatal(logger, err, "Convert filepath to absolute",
+			zap.String("old", filename), zap.String("new", filepathNew))
+		filename = filepathNew
+	}
+
+	content, err := ioutil.ReadFile(filename)
+	log.DebugFatal(logger, err, "Read filename")
+
+	dir, err := os.Getwd()
+	log.DebugFatal(logger, err, "Current workdir", zap.String("dir", dir))
+	fileDir := filepath.Dir(filename)
+	if dir != fileDir {
+		err = os.Chdir(fileDir)
+		log.DebugFatal(logger, err, "Chdir to config filename directory")
+		defer func() {
+			err = os.Chdir(dir)
+			log.DebugFatal(logger, err, "Restore workdir to", zap.String("dir", dir))
+		}()
+	}
+	mergeConfigBytes(ctx, c, content, filename)
+}
+
+// hasMeta reports whether path contains any of the magic characters
+// recognized by Match.
+// copy from filepath module
+func hasMeta(path string) bool {
+	magicChars := `*?[`
+	if runtime.GOOS != "windows" {
+		magicChars = `*?[\`
+	}
+	return strings.ContainsAny(path, magicChars)
+}
+
+func mergeConfigBytes(ctx context.Context, c *configType, content []byte, file string) {
+	// for prevent loop by existed included
+	c.General.IncludeConfigs = nil
+
+	meta, err := toml.Decode(string(content), c)
+	if err == nil && len(meta.Undecoded()) > 0 {
+		err = fmt.Errorf("unknown fields: %v", meta.Undecoded())
+	}
+	log.InfoFatal(zc.L(ctx), err, "Parse config file", zap.String("config_file", file))
+
+	if len(c.General.IncludeConfigs) > 0 {
+		includeConfigs := c.General.IncludeConfigs // need save because it will reset while merging
+		for _, file := range includeConfigs {
+			mergeConfigByTemplate(ctx, c, file)
+		}
+	}
 }
