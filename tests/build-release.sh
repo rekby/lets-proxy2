e8a65fcdd9af254d2b0f99d8d07173f0d503c7fc
rekby <rekby@yandex-team.ru>
2021-09-04 11:46:07 +0300

    go install before cd

--- tests/build-release.sh
+++ tests/build-release.sh
@@ -10,8 +10,10 @@ OUTPUT="$ROOT/output"
 
 rm -rf "$TESTBUILD_DIR"
 
-export GOPATH="$TESTBUILD_DIR"
 export PATH="$GOPATH/bin:$PATH"
+go install github.com/mitchellh/gox
+
+export GOPATH="$TESTBUILD_DIR"
 DEST="$GOPATH/src/github.com/rekby/lets-proxy2"
 mkdir -p "$TESTBUILD_DIR/src/github.com/rekby"
 ln -Ts "$ROOT" "$DEST"
@@ -19,8 +21,6 @@ cd "$DEST"
 
 go version
 go env
-
-go install github.com/mitchellh/gox
 
 if [ "$GO_VERSION" = "1.10" ]; then
   OS_ARCH_BUILDS="linux/386 linux/amd64 linux/arm freebsd/386 freebsd/amd64 freebsd/arm windows/386 windows/amd64"
@@ -30,4 +30,6 @@ fi
 
 GITHUB_TAG="${GITHUB_REF/refs\/tags\//}"
 BUILD_TIME=$(TZ=UTC date --rfc-3339=seconds)
-CGO_ENABLED=0 gox -osarch "$OS_ARCH_BUILDS" --ldflags "-X \"main.VERSION=$GITHUB_TAG+build-$GITHUB_RUN_ID, Build time $BUILD_TIME, commit $GITHUB_SHA, $GO_VERSION\"" --output="$OUTPUT/lets-proxy_{{.OS}}_{{.Arch}}" -verbose --rebuild ./cmd/
+
+export CGO_ENABLED=0
+gox -osarch "$OS_ARCH_BUILDS" --ldflags "-X \"main.VERSION=$GITHUB_TAG+build-$GITHUB_RUN_ID, Build time $BUILD_TIME, commit $GITHUB_SHA, $GO_VERSION\"" --output="$OUTPUT/lets-proxy_{{.OS}}_{{.Arch}}" -verbose --rebuild ./cmd/
